767454a8cd87e9c49a47ed9ca3d536b8531cfbc3
pr0gr4m <l4stpr0gr4m@gmail.com>
2018-01-26 05:07:40 +0900

    Add Daemon

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,30 @@
 import PyQt4
 from PyQt4.QtGui import *
 from PyQt4 import uic
-import sys
+from scapy.all import *
+import sys, csv, time, datetime
+from daemon import Daemon
+
+class DNSDaemon(Daemon):
+	def run(self):
+		global auto_inc
+		global dns_main
+		global dns_sub
+		global g_domain
+		global g_cycle
+		global csv_write
+		global csv_file
+		while True:
+			answer = sr1(IP(dst=dns_main)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=domain)),verbose=0,timeout=1)
+			if not answer:
+				answer = sr1(IP(dst=dns_sub)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=domain)),verbose=0,timeout=2)
+				csv_write.writerow([auto_inc, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), domain, answer[DNS][DNSRR].rdata, dns_sub])
+				csv_file.flush()
+			else:
+				csv_write.writerow([auto_inc, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), domain, answer[DNS][DNSRR].rdata, dns_main])
+				csv_file.flush()
+			auto_inc += 1
+			time.sleep(cycle)
 
 form_class = uic.loadUiType("dns.ui")[0]
 
@@ -15,6 +38,7 @@ class MainWindow(QMainWindow, form_class):
 		self.btnCSV.clicked.connect(self.btn_csv)
 		self.btnStart.clicked.connect(self.btn_start)
 		self.btnStop.clicked.connect(self.btn_stop)
+		self.btnRestart.clicked.connect(self.btn_restart)
 
 	def btn_csv(self):
 		csv_path = QFileDialog.getSaveFileName(self, "Save CSV File", "", ".csv") + ".csv"
@@ -37,7 +61,7 @@ class MainWindow(QMainWindow, form_class):
 			error_dialog.showMessage("Check Main DNS Server")
 			error_dialog.exec_()
 			return
-		print mainDns
+		dns_main = mainDns
 
 		subDns = ""
 		if self.dnsGoogleSub.isChecked():
@@ -53,7 +77,7 @@ class MainWindow(QMainWindow, form_class):
 			error_dialog.showMessage("Check Sub DNS Server")
 			error_dialog.exec_()
 			return
-		print subDns
+		dns_sub = subDns
 
 		domain = self.lineDomain.text()
 		if not domain:
@@ -61,7 +85,7 @@ class MainWindow(QMainWindow, form_class):
 			error_dialog.showMessage("Fill the Domain Information")
 			error_dialog.exec_()
 			return
-		print domain
+		g_domain = domain
 
 		cycle = 3600
 		if self.cycleHour.isChecked():
@@ -75,7 +99,7 @@ class MainWindow(QMainWindow, form_class):
 			error_dialog.showMessage("Check Cycle")
 			error_dialog.exec_()
 			return
-		print cycle
+		g_cycle = cycle
 
 		csv_path = self.lineCSV.text()
 		if not csv_path:
@@ -83,13 +107,20 @@ class MainWindow(QMainWindow, form_class):
 			error_dialog.showMessage("Fill the csv file path")
 			error_dialog.exec_()
 			return
-		print csv_path
+		csv_file = open(csv_path, 'w')
+		csv_write = csv.writer(csv_file)
 
+		daemon.start()
 
 	def btn_stop(self):
-		print "tmp"
+		daemon.stop()
+
+	def btn_restart(self):
+		daemon.restart()
 
 if __name__ == "__main__":
+	daemon = DNSDaemon('/tmp/dns-daemon.pid')
+	auto_inc = 1
 	app = QApplication(sys.argv)
 	window = MainWindow()
 	window.show()
